ad454d8b11ee061bf78bb93edbd0252773374ff1
unknown <gerald.leblond@gmail.com>
2011-02-04 01:49:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ndemo::Application.config.secret_token = 'a1937f199c0f6628876053f6c9f97868842d2f52a144b208605d4755d50bbf06d3eb3b4f769fbd27a4d15dc541af1012690ba53fcad1c0b65e7dfe0a34c76d63'
